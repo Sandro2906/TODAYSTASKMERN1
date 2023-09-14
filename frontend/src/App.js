6ec047a95a29560da3c78a7759080a4535a68ec8
Sandro Gataric <132219558+Sandro2906@users.noreply.github.com>
2023-09-14 18:31:13 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import NavBar from "./pages/NavBar";
 import TasksPage from "./pages/TasksPage";
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:4000'
+axios.defaults.baseURL = 'https://todaystaskmern-1-api.vercel.app'
 axios.defaults.credentials = true
 function App() {
   return (
