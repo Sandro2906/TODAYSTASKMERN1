bffd650162b72996deee86735af43a5468b3b064
Sandro Gataric <132219558+Sandro2906@users.noreply.github.com>
2023-09-14 19:31:58 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -58,6 +58,7 @@ try{
    
 })
 
-app.listen(4000,()=>{
-    console.log('Server has started...')
-})
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
